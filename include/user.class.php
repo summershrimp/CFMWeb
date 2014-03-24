4038c2b702a977957db1cd13d573f6ad833b3a02
Summer Zhang <xm1994@gmail.com>
2014-03-24 21:36:41 +0800

    Update Database Struct and Write some code for users api

--- include/user.class.php
+++ include/user.class.php
@@ -1,36 +1,37 @@
 <?php
-if (! defined ( 'IN_CFM' ))
-{
-    die ( 'Hacking attempt' );
+if (! defined('IN_CFM')) {
+    die('Hacking attempt');
 }
 
 class user
 {
-	public $user_id;
+
+    public $user_id;
+
     public function user($wxid)
     {
-    	$user_id=$this->checkLogin($wxid);
-        if(!$userid) $this->ErrorMSG(BADPASS);
+        $user_id = $this -> checkLogin($wxid);
+        if (! $userid) $this -> ErrorMSG(BADPASS);
     }
-    
-    public function getAddress()
+
+    public function getAddress( )
     {
-    	$sql="SELECT * FROM ".$GLOBALS["db"]."user_address Where `user_id` = ".$this->user_id." LIMIT 1";
-    	$arr=$GLOBALS["db"]->getALL($sql);
-    	return $arr;
+        $sql = "SELECT * FROM " . $GLOBALS["db"] -> prefix . "user_address Where `user_id` = " . $this -> user_id . " LIMIT 1";
+        $arr = $GLOBALS["db"] -> getALL($sql);
+        return $arr;
     }
-    
-    
-    
-    
-    public function placeOrder($carts,$address,$tips)
+
+    public function placeOrder($carts, $address, $tips)
     {
-    	$order_id=$this->makeNewOrder($address);
+        $order_id = $this -> makeNewOrder($address);
+        
     }
-    
-    public function makeNewOrder($address)
+
+    public function makeNewOrder($address, $tips)
     {
-    	
+        $ordersn = date("Ymd") + substr(str_pad(20, time()), '0', STR_PAD_LEFT, 12, 20);
+        $sql = "Insert INTO " . $GLOBALS["db"] -> prefix . "order_info (`order_sn`, `user_id`,`order_status`,`shipping_status`,`pay_status`,`address`,`user_phone`,`tips_amount`) " . "VALUES ('" . $ordersn . "','" . $address['user_id'] . "',1,0,0,'" . $address['address'] . "','" . $address['user_phone'] . "','" . $tips . "')";
+        $GLOBALS['db'] -> query($sql);
+        return $GLOBALS['db'] -> insert_id();
     }
-    
 }
--- sql/carryforme.sql
+++ sql/carryforme.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2014 年 03 月 10 日 12:31
+-- 生成日期: 2014 年 03 月 24 日 13:30
 -- 服务器版本: 5.6.12-log
 -- PHP 版本: 5.4.16
 
@@ -97,22 +97,18 @@ CREATE TABLE IF NOT EXISTS `cfm_ants` (
   `last_login` int(11) unsigned NOT NULL DEFAULT '0',
   `last_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `last_ip` varchar(15) NOT NULL DEFAULT '',
-  `visit_count` smallint(5) unsigned NOT NULL DEFAULT '0',
-  `user_rank` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `is_special` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `pic_url` text,
   `salt` varchar(10) DEFAULT NULL,
   `parent_id` mediumint(9) NOT NULL DEFAULT '0',
   `flag` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `alias` varchar(60) NOT NULL,
-  `qq` varchar(20) NOT NULL,
   `mobile_phone` varchar(20) NOT NULL,
   `is_validated` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `credit_line` decimal(10,2) unsigned NOT NULL,
   `passwd_question` varchar(50) DEFAULT NULL,
   `passwd_answer` varchar(255) DEFAULT NULL,
   `last_lon` varchar(20) NOT NULL,
   `last_lat` varchar(20) NOT NULL,
-  `openid` varchar(32) NOT NULL,
+  `ant_online` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`ant_id`),
   UNIQUE KEY `ant_name` (`ant_name`),
   UNIQUE KEY `ant_id` (`ant_id`),
@@ -121,6 +117,23 @@ CREATE TABLE IF NOT EXISTS `cfm_ants` (
   KEY `email` (`email`),
   KEY `parent_id` (`parent_id`),
   KEY `flag` (`flag`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `cfm_ants_log`
+--
+
+CREATE TABLE IF NOT EXISTS `cfm_ants_log` (
+  `log_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `log_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_id` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `log_info` varchar(255) NOT NULL DEFAULT '',
+  `ip_address` varchar(15) NOT NULL DEFAULT '',
+  PRIMARY KEY (`log_id`),
+  KEY `log_time` (`log_time`),
+  KEY `user_id` (`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -146,16 +159,12 @@ CREATE TABLE IF NOT EXISTS `cfm_customers` (
   `last_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `last_ip` varchar(15) NOT NULL DEFAULT '',
   `visit_count` smallint(5) unsigned NOT NULL DEFAULT '0',
-  `user_rank` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `is_special` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `salt` varchar(10) DEFAULT NULL,
-  `parent_id` mediumint(9) NOT NULL DEFAULT '0',
   `flag` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `alias` varchar(60) NOT NULL,
   `qq` varchar(20) NOT NULL,
   `mobile_phone` varchar(20) NOT NULL,
   `is_validated` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `credit_line` decimal(10,2) unsigned NOT NULL,
   `passwd_question` varchar(50) DEFAULT NULL,
   `passwd_answer` varchar(255) DEFAULT NULL,
   `last_lon` varchar(20) NOT NULL,
@@ -167,37 +176,16 @@ CREATE TABLE IF NOT EXISTS `cfm_customers` (
   UNIQUE KEY `email_2` (`email`),
   UNIQUE KEY `mobile_phone` (`mobile_phone`),
   KEY `email` (`email`),
-  KEY `parent_id` (`parent_id`),
   KEY `flag` (`flag`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `cfm_order_action`
+-- 表的结构 `cfm_order_details`
 --
 
-CREATE TABLE IF NOT EXISTS `cfm_order_action` (
-  `action_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `order_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `action_user` varchar(30) NOT NULL DEFAULT '',
-  `order_status` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `shipping_status` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `pay_status` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `action_place` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `action_note` varchar(255) NOT NULL DEFAULT '',
-  `log_time` int(11) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`action_id`),
-  KEY `order_id` (`order_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `cfm_order_goods`
---
-
-CREATE TABLE IF NOT EXISTS `cfm_order_goods` (
+CREATE TABLE IF NOT EXISTS `cfm_order_details` (
   `rec_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `order_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `goods_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
@@ -205,15 +193,7 @@ CREATE TABLE IF NOT EXISTS `cfm_order_goods` (
   `goods_sn` varchar(60) NOT NULL DEFAULT '',
   `product_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `goods_number` smallint(5) unsigned NOT NULL DEFAULT '1',
-  `market_price` decimal(10,2) NOT NULL DEFAULT '0.00',
   `goods_price` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `goods_attr` text NOT NULL,
-  `send_number` smallint(5) unsigned NOT NULL DEFAULT '0',
-  `is_real` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `extension_code` varchar(30) NOT NULL DEFAULT '',
-  `parent_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `is_gift` smallint(5) unsigned NOT NULL DEFAULT '0',
-  `goods_attr_id` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`rec_id`),
   KEY `order_id` (`order_id`),
   KEY `goods_id` (`goods_id`)
@@ -232,72 +212,45 @@ CREATE TABLE IF NOT EXISTS `cfm_order_info` (
   `order_status` tinyint(1) unsigned NOT NULL DEFAULT '0',
   `shipping_status` tinyint(1) unsigned NOT NULL DEFAULT '0',
   `pay_status` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `consignee` varchar(60) NOT NULL DEFAULT '',
-  `country` smallint(5) unsigned NOT NULL DEFAULT '0',
-  `province` smallint(5) unsigned NOT NULL DEFAULT '0',
-  `city` smallint(5) unsigned NOT NULL DEFAULT '0',
-  `district` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `ant_id` varchar(60) NOT NULL,
   `address` varchar(255) NOT NULL DEFAULT '',
-  `zipcode` varchar(60) NOT NULL DEFAULT '',
-  `tel` varchar(60) NOT NULL DEFAULT '',
-  `mobile` varchar(60) NOT NULL DEFAULT '',
-  `email` varchar(60) NOT NULL DEFAULT '',
-  `best_time` varchar(120) NOT NULL DEFAULT '',
-  `sign_building` varchar(120) NOT NULL DEFAULT '',
-  `postscript` varchar(255) NOT NULL DEFAULT '',
-  `shipping_id` tinyint(3) NOT NULL DEFAULT '0',
-  `shipping_name` varchar(120) NOT NULL DEFAULT '',
-  `pay_id` tinyint(3) NOT NULL DEFAULT '0',
-  `pay_name` varchar(120) NOT NULL DEFAULT '',
-  `how_oos` varchar(120) NOT NULL DEFAULT '',
-  `how_surplus` varchar(120) NOT NULL DEFAULT '',
-  `pack_name` varchar(120) NOT NULL DEFAULT '',
-  `card_name` varchar(120) NOT NULL DEFAULT '',
-  `card_message` varchar(255) NOT NULL DEFAULT '',
-  `inv_payee` varchar(120) NOT NULL DEFAULT '',
-  `inv_content` varchar(120) NOT NULL DEFAULT '',
+  `user_phone` varchar(60) NOT NULL DEFAULT '',
+  `pay_id` tinyint(20) NOT NULL DEFAULT '0',
   `goods_amount` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `shipping_fee` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `insure_fee` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `pay_fee` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `pack_fee` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `card_fee` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `money_paid` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `surplus` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `integral` int(10) unsigned NOT NULL DEFAULT '0',
-  `integral_money` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `bonus` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `order_amount` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `from_ad` smallint(5) NOT NULL DEFAULT '0',
-  `referer` varchar(255) NOT NULL DEFAULT '',
-  `add_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `tips_amount` decimal(10,2) NOT NULL,
+  `add_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `confirm_time` int(10) unsigned NOT NULL DEFAULT '0',
   `pay_time` int(10) unsigned NOT NULL DEFAULT '0',
   `shipping_time` int(10) unsigned NOT NULL DEFAULT '0',
-  `pack_id` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `card_id` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `bonus_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `invoice_no` varchar(255) NOT NULL DEFAULT '',
-  `extension_code` varchar(30) NOT NULL DEFAULT '',
-  `extension_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `to_buyer` varchar(255) NOT NULL DEFAULT '',
-  `pay_note` varchar(255) NOT NULL DEFAULT '',
-  `agency_id` smallint(5) unsigned NOT NULL,
-  `inv_type` varchar(60) NOT NULL,
-  `tax` decimal(10,2) NOT NULL,
-  `is_separate` tinyint(1) NOT NULL DEFAULT '0',
-  `parent_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `discount` decimal(10,2) NOT NULL,
   PRIMARY KEY (`order_id`),
   UNIQUE KEY `order_sn` (`order_sn`),
   KEY `user_id` (`user_id`),
   KEY `order_status` (`order_status`),
   KEY `shipping_status` (`shipping_status`),
   KEY `pay_status` (`pay_status`),
-  KEY `shipping_id` (`shipping_id`),
-  KEY `pay_id` (`pay_id`),
-  KEY `extension_code` (`extension_code`,`extension_id`),
-  KEY `agency_id` (`agency_id`)
+  KEY `pay_id` (`pay_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `cfm_payment`
+--
+
+CREATE TABLE IF NOT EXISTS `cfm_payment` (
+  `pay_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `ant_id` int(11) NOT NULL,
+  `value` decimal(10,2) NOT NULL,
+  `creat_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `pay_time` timestamp NULL DEFAULT NULL,
+  `finished` tinyint(1) NOT NULL DEFAULT '0',
+  `check_id` varchar(32) NOT NULL,
+  PRIMARY KEY (`pay_id`),
+  KEY `user_id` (`user_id`),
+  KEY `order_id` (`order_id`),
+  KEY `ant_id` (`ant_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -355,6 +308,8 @@ CREATE TABLE IF NOT EXISTS `cfm_shop` (
   `owner_id` int(11) DEFAULT NULL,
   `shop_name` varchar(255) NOT NULL,
   `shop_desc` varchar(512) DEFAULT NULL,
+  `shop_pos` text,
+  `shop_phone` varchar(13) DEFAULT NULL,
   `shop_lon` text,
   `shop_lat` text,
   PRIMARY KEY (`shop_id`),
@@ -379,6 +334,22 @@ CREATE TABLE IF NOT EXISTS `cfm_shop_goods` (
   `good_name` text NOT NULL,
   `good_desc` int(11) NOT NULL,
   PRIMARY KEY (`good_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `cfm_user_address`
+--
+
+CREATE TABLE IF NOT EXISTS `cfm_user_address` (
+  `addr_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_phone` varchar(13) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `user_lon` text NOT NULL,
+  `user_lat` text NOT NULL,
+  PRIMARY KEY (`addr_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

2c2a64c1f46cb3eba44cae847412339997de2571
Summer Zhang <xm1994@gmail.com>
2014-03-24 20:16:34 +0800

    Some Unfinished Code

--- include/user.class.php
+++ include/user.class.php
@@ -6,9 +6,31 @@ if (! defined ( 'IN_CFM' ))
 
 class user
 {
-    function user($username,$password)
+	public $user_id;
+    public function user($wxid)
     {
-        $this->checkLogin($username,$password);
-        
+    	$user_id=$this->checkLogin($wxid);
+        if(!$userid) $this->ErrorMSG(BADPASS);
     }
+    
+    public function getAddress()
+    {
+    	$sql="SELECT * FROM ".$GLOBALS["db"]."user_address Where `user_id` = ".$this->user_id." LIMIT 1";
+    	$arr=$GLOBALS["db"]->getALL($sql);
+    	return $arr;
+    }
+    
+    
+    
+    
+    public function placeOrder($carts,$address,$tips)
+    {
+    	$order_id=$this->makeNewOrder($address);
+    }
+    
+    public function makeNewOrder($address)
+    {
+    	
+    }
+    
 }
